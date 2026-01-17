54f992b1acb18700a6ab1c3a8ff114bb22692961
Thejanu Dileepa <156946119+thejanubandarigoda@users.noreply.github.com>
2026-01-17 12:00:49 +0530

    Update i2s_mic_test.ino

--- i2s_mic_test.ino
+++ i2s_mic_test.ino
@@ -1,8 +1,8 @@
 #include <driver/i2s.h>
 
 // you shouldn't need to change these settings
-#define SAMPLE_BUFFER_SIZE 512
-#define SAMPLE_RATE 8000
+#define SAMPLE_BUFFER_SIZE 512  
+#define SAMPLE_RATE 8000  
 // most microphones will probably default to left channel but you may need to tie the L/R pin low
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // either wire your microphone to the same pins or change these to match your wiring
